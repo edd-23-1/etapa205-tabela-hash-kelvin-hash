2822de389474415cb00167ba104e6e25a5c69ad8
Kelvin <kelvinmarquesk@gmail.com>
2023-07-27 17:25:28 -0300

    Implementado o método insert()

--- tabela_hash.py
+++ tabela_hash.py
@@ -71,7 +71,33 @@ class TabelaHashEnderecamentoAberto:
     # Caso a tabela esteja cheia, retorna False
     def insert(self, chave, valor) -> None:
         # implementação do método
-        pass
+        if self.__qtd >= self.__tamanho:
+            return False  # return False se estiver cheia e não for possível inserir o item
+
+        # cria um novo objeto No com a chave e valor recebido(s)
+        novoNo = No(chave, valor)
+
+        # Obtém o índice onde o item (Nó) associado à chave pode ser inserido na tabela hash
+        position = self.__find(chave)
+
+        # verificand se a posição position está vazia ou contém um item que foipreviamente removido (chave = LIBERADO)
+        if self.__tabela[position] is None or self.__tabela[position].chave == self.LIBERADO:
+            # inserindo o novoNo na tabela_hash na posição position
+            self.__tabela[position] = novoNo
+            self.__qtd += 1  # incrementa a quantidade de elementos na tabela
+            return True  # return True indicando a correta inserção
+
+        # se a posição position não estiver vazia, use a sondagem quadrática para encontrar uma posição vazia na tabela.
+        for tentativa in range(1, self.__tamanho):
+            position = self.__hash2(chave, tentativa)
+
+            if self.__tabela[position] is None or self.__tabela[position].chave == self.LIBERADO:
+                self.__tabela[position] = novoNo
+                self.__qtd += 1
+                return True
+
+        # se não encontrar posição vazia após a sondagem quadrática, a tabela_hash está cheia e irá retornar False.
+        return False
     
 
     # Remove um dado da tabela inserindo None na posição da 
